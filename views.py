22cfdd4f6bbafe96b4934f120039d4f58bba27af
Jorge <aldea.diaguita@gmail.com>
2011-09-27 22:41:45 -0300

    agregado jquery 1.6.4

--- views.py
+++ views.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from calcs.forms import *
 
 def home(request):
-    return render(request, "index.html")
+    
+    chill_form = ChillForm()
+    heat_form = HeatForm()
+    evapo_form = EvapoForm()
+    dicc = {"chill_form": chill_form, "heat_form": heat_form, 
+    "evapo_form": evapo_form,}
+    
+    return render(request, "index.html", dicc)

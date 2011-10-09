5c571acbc0eac4ffb7337edb681cabe3de58ac73
Jorge <aldea.diaguita@gmail.com>
2011-10-08 23:32:00 -0300

    Iniciando test de formularios

--- calcs/tests.py
+++ calcs/tests.py
@@ -6,7 +6,6 @@ class IndexTestCase(TestCase):
         
         '''Test to probe existence of each page
         '''
-        
         resp_index = self.client.get('/')
         resp_index_calcs = self.client.get('/calcs/')
         resp_index_chill = self.client.get('/calcs/chill/')
@@ -17,4 +16,15 @@ class IndexTestCase(TestCase):
         self.assertEqual(resp_index_chill.status_code, 200)
         self.assertEqual(resp_index_heat.status_code, 200)
         self.assertEqual(resp_index_evapo.status_code, 200)
+    
+    def test_forms(self):
         
+        from calcs.forms import ChillForm, HeatForm
+        
+        '''Test to probe chill, heat and evapo forms
+        '''
+        #first test chill form
+        url = "/calcs/chill/"
+        chill_good_data = {"chill_method": "horas_frio", "temp": 6}
+        form = ChillForm(chill_good_data)
+        self.assertTrue(form.is_valid())

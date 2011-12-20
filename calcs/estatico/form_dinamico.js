a46cd3ef22c2bc1ee55551db72eec3c896ead91f
Jorge <aldea.diaguita@gmail.com>
2011-12-19 23:05:25 -0300

    Comenzando a trastear con el javascript para el form dinámico

--- calcs/estatico/form_dinamico.js
+++ calcs/estatico/form_dinamico.js
@@ -1,7 +1,8 @@
 (function($){
-    $("#id_chosen_method").change(function(event){
-        var mensaje = $("#id_chosen_method").val();
-        alert(mensaje);
+    explicacion = function() {
+        $("#id_chosen_method").change(function(event){
+            var mensaje = $("#id_chosen_method").val();
+            alert(mensaje);
+        });
     };
 })(jQuery);
-

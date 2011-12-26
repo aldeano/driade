94371dc0a9a9a93e6c3343128566122244e927e3
Jorge <aldea.diaguita@gmail.com>
2011-12-25 21:32:58 -0300

    Form frio con campos dinámicos

--- calcs/estatico/form_dinamico.js
+++ calcs/estatico/form_dinamico.js
@@ -1,8 +1,21 @@
 (function($){
     explicacion = function() {
-        $("#id_chosen_method").change(function(event){
-            var mensaje = $("#id_chosen_method").val();
-            alert(mensaje);
+        $("#id_chosen_method").change(function(){
+            var eleccion = $("#id_chosen_method").val();
+            if (eleccion != "horas_frio")
+            {
+                $("#id_base_temp").hide("slow");
+                $("#id_sup_temp").hide("slow");
+                $("label[for|='id_base_temp']").hide("slow");
+                $("label[for|='id_sup_temp']").hide("slow");
+            }
+            else
+            {
+                $("#id_base_temp").show("slow");
+                $("#id_sup_temp").show("slow");
+                $("label[for|='id_base_temp']").show("slow");
+                $("label[for|='id_sup_temp']").show("slow");
+            }
         });
     };
 })(jQuery);
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,7 +7,7 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^calcs/', include('driade.calcs.urls')),
-    url(r'^$', direct_to_template, {'template': 'index.html'}, name="home"),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
     # url(r'^driade/', include('driade.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
